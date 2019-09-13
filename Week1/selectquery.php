aca40e86cbba26a245a870d91d43f84a3f063175
Michael Tang <michaelandrewtang@gmail.com>
2019-09-13 10:54:19 -0400

    Update selectquery.php

--- Week1/selectquery.php
+++ Week1/selectquery.php
@@ -1,27 +1,30 @@
 <?php
 $servername = "localhost";
-$username = "username";
+$username = "root";
 $password = "password";
-
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=employees", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
+
+        $data = $conn->query("SELECT * FROM departments")->fetchAll();
+        foreach ($data as $row) {
+                echo $row['dept_name']."<br />\n";
+        }
+
+        // try to fix the error below    
+        $data = $conn->query("SELECT count(1) as employees_count FROM employees")->fetchAll();
+        foreach ($data as $row) {
+                echo $row['employees count']."<br />\n";
+        }
+    
     }
 catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
     }
 
-
-$data = $pdo->query("SELECT * FROM users")->fetchAll();
-
-foreach ($data as $row) {
-    echo $row['name']."<br />\n";
-}
-
 // Close database connection
 $conn = null;
-
 ?>
